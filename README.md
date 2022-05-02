585bbd94bc9e1496c8390c51110f66652bd0c47c
kamalC5196 <kamal.bob@icloud.com>
2022-05-02 10:26:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-sk8q4s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-sk8q4s)
---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const explorer = [
+    {
+      name: 'public',
+      isFolder: true,
+      files: [
+        {
+          name: 'index.html',
+          isFolder: false,
+          files: [],
+        },
+      ],
+    },
+    {
+      name: 'src',
+      isFolder: true,
+      files: [
+        {
+          name: 'App.js',
+          isFolder: false,
+          files: [],
+        },
+        {
+          name: 'index.js',
+          isFolder: false,
+          files: [],
+        },
+        {
+          name: 'style.css',
+          isFolder: false,
+          files: [],
+        },
+        {
+          name: 'components',
+          isFolder: true,
+          files: [
+            {
+              name: 'c.js',
+              isFolder: false,
+              files: [],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'package.json',
+      isFolder: false,
+      files: [],
+    },
+  ];
+  return (
+    <>
+      {explorer.map((e, i) => (
+        <Display explorer={e} />
+      ))}
+    </>
+  );
+}
+
+function Display({ explorer }) {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      {explorer.isFolder ? (
+        <h2
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          {explorer.name}
+        </h2>
+      ) : (
+        <p>{explorer.name}</p>
+      )}
+
+      <ul style={{ display: show ? 'block' : 'none' }}>
+        {explorer.files.map((e, i) => (
+          <Display key={i} explorer={e} />
+        ))}
+      </ul>
+    </>
+  );
+}
